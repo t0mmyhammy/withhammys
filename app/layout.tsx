01c055831ac2edc0d5ed4c651f04c9a52390797f
Tom Hamilton <tom@withtrusted.com>
2025-07-07 12:29:17 -0400

    #feature Inline critical CSS and font-face rules in <head> for all pages. Improves LCP/FCP and eliminates render-blocking font/style requests.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,13 +20,100 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
-          rel="stylesheet"
-        />
-        <link href="https://fonts.cdnfonts.com/css/gotham" rel="stylesheet" />
+        <style>{`
+          @font-face {
+            font-family: 'DM Sans';
+            src: url('/fonts/DMSans-Regular.woff2') format('woff2');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: 'DM Sans';
+            src: url('/fonts/DMSans-Italic.woff2') format('woff2');
+            font-weight: 400;
+            font-style: italic;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: 'Discount Black';
+            src: url('/fonts/Discount-Black.otf') format('opentype');
+            font-weight: 900;
+            font-style: normal;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: 'Discount Heavy';
+            src: url('/fonts/Discount-Heavy.otf') format('opentype');
+            font-weight: 900;
+            font-style: normal;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: 'Discount Bold Slant';
+            src: url('/fonts/Discount-BoldSlant.otf') format('opentype');
+            font-weight: 700;
+            font-style: italic;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: 'Discount Medium';
+            src: url('/fonts/Discount-Medium.otf') format('opentype');
+            font-weight: 500;
+            font-style: normal;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: 'Discount Regular Slant';
+            src: url('/fonts/Discount-RegularSlant.otf') format('opentype');
+            font-weight: 400;
+            font-style: italic;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: 'Discount Light';
+            src: url('/fonts/Discount-Light.otf') format('opentype');
+            font-weight: 300;
+            font-style: normal;
+            font-display: swap;
+          }
+          :root {
+            --background: 0 0% 100%;
+            --foreground: 0 0% 3.9%;
+          }
+          body {
+            font-family: 'DM Sans', Arial, Helvetica, sans-serif;
+            font-weight: 400;
+            font-style: normal;
+            background: hsl(var(--background));
+            color: hsl(var(--foreground));
+          }
+          h1 {
+            font-family: 'Discount Heavy', sans-serif;
+            font-weight: 900;
+            font-style: normal;
+          }
+          h2 {
+            font-family: 'Discount Bold Slant', sans-serif;
+            font-weight: 700;
+            font-style: italic;
+          }
+          h3, .nav-menu {
+            font-family: 'Discount Medium', sans-serif;
+            font-weight: 500;
+            font-style: normal;
+          }
+          h4, .quote, .pullout {
+            font-family: 'Discount Regular Slant', sans-serif;
+            font-weight: 400;
+            font-style: italic;
+          }
+          h5, .meta, .caption {
+            font-family: 'Discount Light', sans-serif;
+            font-weight: 300;
+            font-style: normal;
+          }
+        `}</style>
         <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
         <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
         <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
@@ -36,6 +123,8 @@ export default function RootLayout({
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
           strategy="beforeInteractive"
         />
+        {/* Gotham font link commented out; Gotham is no longer used in the codebase. */}
+        {/* <link href="https://fonts.cdnfonts.com/css/gotham" rel="stylesheet" /> */}
       </head>
       <body className={inter.className} style={{ fontFamily: "DM Sans, sans-serif" }}>
         {children}
